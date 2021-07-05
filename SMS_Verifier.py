8bb58c296a5423b25f7923799a0116701566931c
Jerry Mathew <86571727+JMat47@users.noreply.github.com>
2021-07-06 01:37:08 +0530

    Beta 1

--- SMS_Verifier.py
+++ SMS_Verifier.py
@@ -20,11 +20,11 @@ driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
 driver.get("https://messages.google.com/web/authentication")
 
 #names_list = ["9176684086" ,"9962055580","73585 79588","98845 20487"]
-names_list = ["8838210069"]
+#names_list = ["9384832755","9840656355"]
 
-# donorData = pd.read_csv(r"Final_Data.csv")
-# names_list = [list(row) for row in donorData.values]
-# print(names_list)
+donorData = pd.read_csv(r"Sent_log.csv")
+names_list = [list(row) for row in donorData.values]
+print(names_list)
 
 wait_for_verification = input()
 sleep(2)
@@ -36,15 +36,32 @@ for name in names_list:
     sleep(1)
 
     search_for_chat = driver.find_element_by_xpath("/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-new-conversation-container/mw-new-conversation-sub-header/div/div[2]/mw-contact-chips-input/div/mat-chip-list/div/input")
-    search_for_chat.send_keys(name)
+    search_for_chat.send_keys(name[2])
     search_for_chat.send_keys("\n")
 
-    sleep(5)
+    sleep(3)
 
-    # recieved_text = driver.find_element_by_xpath("/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div/div[1]/div/mws-messages-list/mws-bottom-anchored")
-    # print(recieved_text.text)
+    recieved_text = driver.find_elements_by_xpath("/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div/div[1]/div/mws-messages-list/mws-bottom-anchored/div/div/div/mws-message-wrapper")
+    recieved_text = recieved_text[1:]
+    recieved_text =[x.text for x in recieved_text]
+    t = "".join(recieved_text).lower()
+    if("yes" in t and "no" in t):
+        name.append("Ambigious")
+    elif("wrong" in t):
+        name.append("Wrong INFO")
+    elif("yes" in t):
+        name.append("Donor")
+    elif("no" in t):
+        name.append("Not a Donor")
+    else:
+        name.append("No Valid reply")
 
-    recieved_text = driver.find_elements_by_css_selector("div _ngcontent-vgx-c166 [class='text-msg ng-star-inserted']")
-    print(recieved_text)
+    data = [name]
+    column_names = ["Name", "BloodGroup", "PhoneNo","District","City","SMS","Donor"]
+    df = pd.DataFrame(data, columns=column_names)
+    df.to_csv('Verify_log.csv',mode='a', index=False, header=False)
 
+
+sleep(5)
+driver.quit()
    
